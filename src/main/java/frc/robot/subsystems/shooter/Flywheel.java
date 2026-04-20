868b7575f25bcb3c53d3bac2253fac9474c4d77b
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2026-04-20 16:21:38 -0400

    stuff -warren

--- src/main/java/frc/robot/subsystems/shooter/Flywheel.java
+++ src/main/java/frc/robot/subsystems/shooter/Flywheel.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+import java.util.function.Supplier;
 
 public class Flywheel extends SubsystemBase {
     private final FlywheelIO io;
@@ -13,5 +19,38 @@ public class Flywheel extends SubsystemBase {
     @Override
     public void periodic() {
         io.updateInputs(inputs);
+        Logger.processInputs("Flywheel",inputs);
+    }
+
+    public AngularVelocity getVelocity() {
+        return inputs.rpm;
+    }
+
+    public void setVelocity(AngularVelocity rpm) {
+        io.setVelocity(rpm);
+    }
+
+    public void setVoltage(Voltage voltage) {
+        io.setVoltage(voltage);
+    }
+
+    public void stop() {
+        io.stop();
+    }
+
+    public Command runVoltage(Supplier<Voltage> voltage) {
+        return runEnd(()->setVoltage(voltage.get()), this::stop);
+    }
+
+    private Command runVelocity(Supplier<AngularVelocity> rpm) {
+        return runEnd(
+                ()->setVelocity(rpm.get()),
+                this::stop
+        ); //maybe if need the goofy goal thing in example ill put that
+        //goofy
+    }
+
+    public Command commandStop() {
+        return run(this::stop);
     }
 }
--- src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
@@ -1,14 +1,24 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
 import org.littletonrobotics.junction.AutoLog;
+
+import static edu.wpi.first.units.Units.RadiansPerSecond;
 
 public interface FlywheelIO {
     @AutoLog
     public static class FlywheelIOInputs {
-        
+        public AngularVelocity rpm=RadiansPerSecond.of(0); // rotations per minute
     }
 
     public default void updateInputs(FlywheelIOInputsAutoLogged inputs) {
 
     }
+
+    public default void setVelocity(AngularVelocity rpm) {}
+
+    public default void setVoltage(Voltage voltage) {}
+
+    public default void stop() {}
 }
