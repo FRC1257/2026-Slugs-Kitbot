a06807fdb7b7a2b013814181bba8e34ff09614cc
laelHI <lgittens@ucvts.org>
2026-04-20 16:32:40 -0400

    stuff

--- src/main/java/frc/robot/subsystems/Kicker/Kicker.java
+++ src/main/java/frc/robot/subsystems/Kicker/Kicker.java
@@ -1,4 +1,79 @@
-public class Kicker {
+package frc.robot.subsystems.Kicker;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Robot;
+import frc.robot.util.misc.LoggedTunableNumber;
+
+package frc.robot.subsystems.Kicker;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Robot;
+import frc.robot.util.misc.LoggedTunableNumber;
+
+public class Kicker extends SubsystemBase {
+
+    private static final LoggedTunableNumber Kp = new LoggedTunableNumber("Kicker/Kp", KickerConstants.KICKER_KP);
+    private static final LoggedTunableNumber Ki = new LoggedTunableNumber("Kicker/Ki", KickerConstants.KICKER_KI);
+    private static final LoggedTunableNumber Kd = new LoggedTunableNumber("Kicker/Kd", KickerConstants.KICKER_KD);
     
-}
+    private static final LoggedTunableNumber Ks = new LoggedTunableNumber("Kicker/Ks", KickerConstants.KICKER_KS);
+    private static final LoggedTunableNumber Kv = new LoggedTunableNumber("Kicker/Kv", KickerConstants.KICKER_KV);
+
+    private static final LoggedTunableNumber tolerance = new LoggedTunableNumber("Kicker/Tolerance", KickerConstants.KICKER_VELOCITY_TOLERANCE);
+
+    private static final LoggedTunableNumber kickerIntakeVelocity = new LoggedTunableNumber("Kicker/IntakeVelocity", KickerConstants.KICKER_INTAKE_VELOCITY.magnitude()); 
+    private static final LoggedTunableNumber kickerOuttakeVelocity = new LoggedTunableNumber("Kicker/OuttakeVelocity", KickerConstants.KICKER_OUTTAKE_VELOCITY.magnitude());
+
+    private final KickerIO io;
+    private KickerIOInputsAutoLogged inputs = new KickerIOInputsAutoLogged();
+
+    private final Debouncer connectedDebouncer = 
+        new Debouncer(0.5, DebounceType.kFalling);
+    
+    private final Alert disconnected;
+
+    private double goalVelocity = 0.0;
+
+    public Kicker(KickerIO io) {
+        this.io = io;
+
+        disconnected = new Alert("KICKER MOTOR DISCONNECTED", AlertType.kError);
+        
+        if(disconnected){ 
+             Logger.recordOutput("",disconnected); 
+
+    }  
+
 //copy from tanish 
--- src/main/java/frc/robot/subsystems/Kicker/KickerIO.java
+++ src/main/java/frc/robot/subsystems/Kicker/KickerIO.java
@@ -1,3 +1,17 @@
-public class KickerConstants {
-    
+public class KickerIO {
+    public static class KickerIOInputs {
+        public boolean kickerLoaded = false;// this was kicker connected i think thats what it means
+        public Voltage voltage = Voltage.of(0.0); //Wrapper class for voltage is like double tho
+        public AngularVelocity angularVelocity = RadiansPerSecond.of(0.0); //idk
+        public Angle angle = Radians.of(0.0); 
+        public Currrent kickerCurrent =  Amps.of(0.0); //
+    }
+    public default void updateInputs(KickerIOInputs inputs) {}
+    public default void setVoltage(Voltage voltage) {}
+    public default void setVelocity(AngularVelocity velocity) {}
+    public default void stop() {}
+    public default void setVelocity(KickerIOInputs inputs) {}
+    public default void setVelocity(KickerIOInputs inputs) {}
+    public default void setVelocity(KickerIOInputs inputs) {}
+
 }
