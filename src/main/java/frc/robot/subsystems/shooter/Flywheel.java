4ee7b8a3fd66f2dfcabb333f1d899c73065b175b
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2026-04-20 15:52:51 -0400

    did random stuff -warren

--- src/main/java/frc/robot/subsystems/shooter/Flywheel.java
+++ src/main/java/frc/robot/subsystems/shooter/Flywheel.java
@@ -3,4 +3,15 @@ package frc.robot.subsystems.shooter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Flywheel extends SubsystemBase {
+    private final FlywheelIO io;
+    private final FlywheelIOInputsAutoLogged inputs=new FlywheelIOInputsAutoLogged();
+
+    public Flywheel(FlywheelIO io) {
+        this.io=io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+    }
 }
--- src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
@@ -1,4 +1,14 @@
 package frc.robot.subsystems.shooter;
 
-public class FlywheelIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface FlywheelIO {
+    @AutoLog
+    public static class FlywheelIOInputs {
+
+    }
+
+    public default void updateInputs(FlywheelIOInputsAutoLogged inputs) {
+
+    }
 }
