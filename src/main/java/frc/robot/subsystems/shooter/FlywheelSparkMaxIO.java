712ad901976603f94fa57ea0f1a2409104cd452b
The-pranker <william.y.chen2011@gmail.com>
2026-04-20 16:00:52 -0400

    Update FlywheelSparkMaxIO.java
    i made stuff for sparkmaxio

--- src/main/java/frc/robot/subsystems/shooter/FlywheelSparkMaxIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelSparkMaxIO.java
@@ -1,4 +1,12 @@
 package frc.robot.subsystems.shooter;
 
-public class FlywheelSparkMaxIO {
+public class FlywheelSparkMaxIO implements FlywheelIO {
+
+  private final SparkMax motor;
+  private final RelativeEncoder encoder;
+  private final SparkMaxConfig flywheelConfig;
+
+  public FlywheelSparkMaxIO() {
+  
+  }
 }
