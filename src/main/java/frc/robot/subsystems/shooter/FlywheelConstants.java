1e7738c937b6d2ad6dd07d516322754d87316005
The-pranker <william.y.chen2011@gmail.com>
2026-04-20 16:07:11 -0400

    Create FlywheelConstants.java and add things to it
    i added some constants

---
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelConstants.java
@@ -0,0 +1,8 @@
+public class FlywheelConstants() {
+
+  public static final double flywheel_kp = 0.0; //placeholder
+  public static final double flywheel_ki = 0.0;
+  public static final double flywheel_kd = 0.0;
+
+  public static final int flywheel_motor_id = 1; //placceholder again
+}
