879ffae8a08694fab82f9cda0d16c141093eb34c
oorekie-jpg <oorekie@ucvts.org>
2026-04-20 16:03:43 -0400

    Format FlywheelIOInputs class for consistency

--- src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
@@ -5,7 +5,7 @@ import org.littletonrobotics.junction.AutoLog;
 public interface FlywheelIO {
     @AutoLog
     public static class FlywheelIOInputs {
-
+        
     }
 
     public default void updateInputs(FlywheelIOInputsAutoLogged inputs) {
