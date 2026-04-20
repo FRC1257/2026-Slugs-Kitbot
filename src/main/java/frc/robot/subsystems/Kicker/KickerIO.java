0a54ab4b94d3799b0fc7bef459055318ed09e649
laelHI <lgittens@ucvts.org>
2026-04-20 16:39:00 -0400

    last for today

--- src/main/java/frc/robot/subsystems/Kicker/KickerIO.java
+++ src/main/java/frc/robot/subsystems/Kicker/KickerIO.java
@@ -1,7 +1,9 @@
+
 public class KickerIO {
+    @AutoLog
     public static class KickerIOInputs {
         public boolean kickerLoaded = false;// this was kicker connected i think thats what it means
-        public Voltage voltage = Voltage.of(0.0); //Wrapper class for voltage is like double tho
+        public Voltage voltage = Volts.of(0.0); //Wrapper class for voltage is like double tho
         public AngularVelocity angularVelocity = RadiansPerSecond.of(0.0); //idk
         public Angle angle = Radians.of(0.0); 
         public Currrent kickerCurrent =  Amps.of(0.0); //
@@ -10,8 +12,8 @@ public class KickerIO {
     public default void setVoltage(Voltage voltage) {}
     public default void setVelocity(AngularVelocity velocity) {}
     public default void stop() {}
-    public default void setVelocity(KickerIOInputs inputs) {}
-    public default void setVelocity(KickerIOInputs inputs) {}
-    public default void setVelocity(KickerIOInputs inputs) {}
+    public default void setPID(double kP, double kI, double kD) {}//PID
+    public default void setFF(double kS, double kV) {}
+    public default void setBreakMode(boolean enabled) {} //
 
 }
