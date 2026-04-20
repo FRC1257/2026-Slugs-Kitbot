a2241b2481b733ea2fca8c07f0574615f3483c02
The-pranker <william.y.chen2011@gmail.com>
2026-04-20 16:11:15 -0400

    more to sparkmaxio
    more stuff

--- src/main/java/frc/robot/subsystems/shooter/FlywheelSparkMaxIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelSparkMaxIO.java
@@ -7,6 +7,19 @@ public class FlywheelSparkMaxIO implements FlywheelIO {
   private final SparkMaxConfig flywheelConfig;
 
   public FlywheelSparkMaxIO() {
-  
-  }
+    motor = new SparkMax(FlywheelConstants.flywheel_motor_id, SparkMax.MotorType.kBrushless);
+    encoder = motor.getEncoder();
+    flywheelConfig = new SparkMaxConfig();
+
+    @Override
+    public void setVoltage(Voltage voltage) {
+      motor.setVoltage(voltage);
+    }
+
+    @Override
+    public void stop() {
+      motor.stopMotor();
+    }
+
+    
 }
