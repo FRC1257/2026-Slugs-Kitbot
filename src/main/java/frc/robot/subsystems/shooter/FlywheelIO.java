f7aa2b2c9c990f6db04a5eb6186f82d12ae1ce08
The-pranker <william.y.chen2011@gmail.com>
2026-04-23 16:18:48 -0400

    thingys in update inputs (added new things to it)

--- src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelIO.java
@@ -1,22 +1,26 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.units.measure.Current;
 import org.littletonrobotics.junction.AutoLog;
 
 import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.Volts;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
 
 public interface FlywheelIO {
     @AutoLog
     public static class FlywheelIOInputs {
-        public AngularVelocity rpm=RPM.of(0); // rotations per minute
-        public Voltage voltage=Volts.of(0);
+        public AngularVelocity flywheelRPM=RPM.of(0); // rotations per minute
+        public Voltage flywheelVoltage = Volts.of(0.0);
+        public Temperature flywheelTemperature = Celsius.of(0.0);
+        public Current flywheelCurrent = Amps.of(0.0);
     }
 
-    public default void updateInputs(FlywheelIOInputsAutoLogged inputs) {
-
-    }
+    public default void updateInputs(FlywheelIOInputsAutoLogged inputs) {}
 
     public default void setVelocity(AngularVelocity rpm) {}
 
