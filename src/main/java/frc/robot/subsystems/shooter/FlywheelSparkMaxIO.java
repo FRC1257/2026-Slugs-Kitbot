04ebaa280a8a8e4edf26aa149c1e1853f1520c87
The-pranker <william.y.chen2011@gmail.com>
2026-04-23 15:57:59 -0400

    thingys in update inputs (added new things to it)

--- src/main/java/frc/robot/subsystems/shooter/FlywheelSparkMaxIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FlywheelSparkMaxIO.java
@@ -1,32 +1,54 @@
 package frc.robot.subsystems.shooter;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
+
+import static edu.wpi.first.units.Units.Volts;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Amps;
 
 public class FlywheelSparkMaxIO implements FlywheelIO {
 
   private final SparkMax motor;
   private final RelativeEncoder encoder;
   private final SparkMaxConfig flywheelConfig;
+  private final SparkClosedLoopController controller;
+  private final SimpleMotorFeedforward feedforward;
 
   public FlywheelSparkMaxIO() {
-    motor = new SparkMax(FlywheelConstants.FLYWHEEL_MOTOR_ID, SparkMax.MotorType.kBrushless);
-    encoder = motor.getEncoder();
-    flywheelConfig = new SparkMaxConfig();
-
+      motor = new SparkMax(FlywheelConstants.FLYWHEEL_MOTOR_ID, SparkMax.MotorType.kBrushless);
+      encoder = motor.getEncoder();
+      flywheelConfig = new SparkMaxConfig();
+      feedforward = new SimpleMotorFeedforward(FlywheelConstants.FLYWHEEL_KS, FlywheelConstants.FLYWHEEL_KV);
 
   }
+      @Override
+      public void setVoltage (Voltage voltage){
+          motor.setVoltage(voltage);
+      }
 
-  @Override
-  public void setVoltage(Voltage voltage) {
-    motor.setVoltage(voltage);
-  }
+      @Override
+      public void stop () {
+          motor.stopMotor();
+      }
 
-  @Override
-  public void stop() {
-    motor.stopMotor();
-  }
-    
+      @Override
+      public void setFF ( double ks, double kv){
+          feedforward = new SimpleMotorFeedforward(ks, kv);
+
+      }
+
+      @Override
+      public void updateInputs(FlywheelIOInputs inputs) {
+        inputs.flywheelVoltage = Volts.of(motor.getAppliedOutput() * motor.getBusVoltage());
+        inputs.flywheelCurrent = Amps.of(motor.getOutputCurrent());
+        inputs.flywheelTemperature = Celsius.of(motor.getMotorTemperature());
+      }
+
+
 }
