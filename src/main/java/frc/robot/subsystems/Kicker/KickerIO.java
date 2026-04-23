7f88d571bc1ee45450cef01a7ef7f8d0732a1379
laelHI <lgittens@ucvts.org>
2026-04-23 15:10:49 -0400

    all the work from tanish

--- src/main/java/frc/robot/subsystems/Kicker/KickerIO.java
+++ src/main/java/frc/robot/subsystems/Kicker/KickerIO.java
@@ -1,19 +1,42 @@
+//KickerIO.java 
+package frc.robot.subsystems.Kicker;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RevolutionsPerMinute;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 
 public interface KickerIO {
+  
     @AutoLog
     public static class KickerIOInputs {
-        public boolean kickerLoaded = false;// this was kicker connected i think thats what it means
-        public Voltage voltage = Volts.of(0.0); //Wrapper class for voltage is like double tho
-        public AngularVelocity angularVelocity = RadiansPerSecond.of(0.0); //idk
-        public Angle angle = Radians.of(0.0); 
-        public Currrent kickerCurrent =  Amps.of(0.0); //
+        public boolean kickerConnected = false;
+        public Voltage kickerVoltage = Volts.of(0.0);
+        public AngularVelocity kickerAngularVelocity = RevolutionsPerMinute.of(0.0);
+        public Angle kickerAngle = Radians.of(0.0);
+        public Current kickerCurrent = Amps.of(0.0);
     }
-    public default void updateInputs(KickerIOInputs inputs) {}
+
+public default void updateInputs(KickerIOInputs inputs) {}
+    
     public default void setVoltage(Voltage voltage) {}
+
     public default void setVelocity(AngularVelocity velocity) {}
+
     public default void stop() {}
-    public default void setPID(double kP, double kI, double kD) {}//PID
+
+    public default void setPID(double kP, double kI, double kD) {} 
+    
     public default void setFF(double kS, double kV) {}
-    public default void setBreakMode(boolean enabled) {} //
+
+    public default void setBreakMode(boolean enabled) {}
+
 
 }
--- src/main/java/frc/robot/subsystems/Kicker/KickerIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/Kicker/KickerIOSparkMax.java
@@ -1,4 +1,50 @@
-public class KickerIOSparkMax implements {
-    
+//KickerIOSparkMax.java 
+
+package frc.robot.subsystems.Kicker;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RevolutionsPerMinute;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.REVLibError;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.EncoderConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage; 
+
+ private SparkMax kickerMotor = new SparkMax(KickerConstants.KICKER_MOTOR_ID, SparkMax.MotorType.kBrushless);
+ private SimpleMotorFeedforward feedforward;
+
+
+/*
+Implement the following: 
+1. A new motor class 
+2. Sparkmax Encoder and Closed loop config 
+3.Update Inputs 
+4.Set PID values 
+5. Set Feedforward Mechanism Reseach Revlib API to figure out how to include feedforward mechanism in the closed loop config.  
+*/
+
+private final Spark kicker_motor=new Spark(0) 
+private SimpleMotorFeedforward feedforward;   
+
+ public KickerIOSparkMax() {
+feedforward = new SimpleMotorFeedforward(KickerConstants.KICKER_KS, KickerConstants.KICKER_KV);
+
+
+
+
 }
+
 //copy from tanish 
